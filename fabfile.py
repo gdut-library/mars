4dd916b4c71a1f4ea8b9b9a755706347de786c45
hbc <bcxxxxxx@gmail.com>
2013-09-14 15:16:56 +0800

    fixed typo

--- fabfile.py
+++ fabfile.py
@@ -9,7 +9,7 @@ def sync_server_config():
     with cd('/home/hbc/workshop/gdut-library-server'):
         put('supervisord.conf', 'supervisord.conf')
         put('server.nginx.conf', 'server.nginx.conf')
-        put('configurations/production.py', 'config/production.py')
+        put('configurations/production.py', 'configurations/production.py')
 
 
 def deploy():
